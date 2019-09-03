7fb67fd1cb74ac5be63c6c8585e8bd23886bc745
aurbanow1 <32047123+aurbanow1@users.noreply.github.com>
2019-09-02 21:31:50 -0700

    Update differential_expression.R

--- differential_expression.R
+++ differential_expression.R
@@ -2,39 +2,40 @@ library("DESeq2")
 library("EnhancedVolcano")
 library("pheatmap")
 library("RColorBrewer")
+library("genefilter")
+library("regionReport")
 
-path="FILE_LOCATION"
+path="PATH"
 setwd(path)
-directory <-path
-group=rep(c("c","c_imc"), each=5)
-sampleFiles<-list.files()
+directory<-path
+group=rep(c("zm","ctrl"), each=3)
+files<-list.files()
 
-sampleCondition = group
-
-sampleTable <- data.frame(sampleName = sampleFiles, fileName = sampleFiles, condition = sampleCondition)
- ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
-                                      directory = directory,
-                                        design= ~condition)
+table <- data.frame(sampleName = files, fileName = files, condition = group)
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = table,
+                                       directory = directory,
+                                       design= ~condition)
 
 dds <- DESeq(ddsHTSeq)
 
 keep <- rowSums(counts(dds)) >= 5
 dds <- dds[keep,]
 
-res<-results( dds, contrast=c("condition","c_imc","c"))
+res<-results( dds, contrast=c("condition","zm","ctrl"))
 res<-res[order(res$padj),]
 head(res)
 
-write.csv(as.data.frame(res),file='sim_condition_treated_results_deseq2.csv')
+de<-as.data.frame(res)
+write.csv(de,file='de.csv')
 write.csv(counts(dds, normalized=T),file='normalized_counts.csv')
 write.csv(counts(dds, normalized=F),file='raw_counts.csv')
 
-plotMA(dds,ylim=c(-10,10),main='DESeq2: DE pAdjValue < 0.005')
-dev.copy(png,'deseq2_MAplot.png')
+plotMA(dds,ylim=c(-10,10),main='DE pAdjValue < 0.005')
+dev.copy(png,'DE MAplot.png')
 dev.off()
 
 vsd <- vst(dds, blind=FALSE)
-//vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
+#vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
 rld <- rlog(dds, blind=FALSE)
 head(assay(vsd), 3)
 
@@ -55,46 +56,36 @@ plotPCA(vsd, intgroup=c("condition"))
 dev.copy(png,'pca.png')
 dev.off()
 
-library( "genefilter" )
 topVarGenes <- head( order( rowVars( assay(rld) ), decreasing=TRUE ), 15 )
 
 pheatmap( assay(rld)[ topVarGenes, ], scale="row", 
-     trace="none", dendrogram="column", 
-     col = colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),
-     annotate_col=df )
+          trace="none", dendrogram="column", 
+          col = colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),
+          annotate_col=df )
 
 dev.copy(png,'15-gene-distance.png')
 dev.off()
 
-topVarGenes <- head( order( rowVars( assay(rld) ), decreasing=TRUE ),1000 )
+count_de<-dim(de[de[5]<0.05,])[1]
+topVarGenes <- head( order( rowVars( assay(rld) ), decreasing=TRUE ),count_de )
 
-pheatmap( assay(rld)[ topVarGenes, ], scale="row", 
-     trace="none", dendrogram="column", 
-     col = colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),
-      annotate_col=df  )
+pheatmap(assay(rld)[ topVarGenes, ], scale="row", 
+          trace="none", dendrogram="column", 
+          col = colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),
+          annotate_col=df )
 
 dev.copy(png,'all-gene-distance.png')
 dev.off()
 
 EnhancedVolcano(res,
-                
                 lab = rownames(res),
-                
                 x = "log2FoldChange",
-                
                 y = "pvalue",
-                
                 transcriptPointSize = 1.5,
-                
                 transcriptLabSize = 3.0)
-
 
 dev.copy(png,'volcano_plot.png')
 dev.off()
 
-library(regionReport)
-
 report <- DESeq2Report(dds, project = 'DESeq2 HTML report',
-                        intgroup = c('condition'), outdir='DESeq2-example',output = 'index', theme = theme_bw())
-
-
+                       intgroup = c('condition'), outdir='DE-Report',output = 'index', theme = theme_bw())
